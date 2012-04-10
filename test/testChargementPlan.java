9ea0d69c9356ce56fcfe57ecc5103a08bc25c9f8
Loic Cimon <loic.cimon@laposte.net>
2012-04-10 20:28:20 +0200

    ajout test equals de Station

---
+++ test/testChargementPlan.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Loïc Cimon
+ */
+public class testChargementPlan {
+
+    public testChargementPlan() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testEqualsStation() {
+        assertFalse(new Station("rose").equals(null));
+        
+        assertTrue(new Station("rose").equals(new Station("rose")));
+        assertFalse(new Station("rose").equals(new Station("tulipe")));
+        
+        assertTrue(new Station("rose", new Coordonnee(1.1, 2.1)).equals(new Station("rose")));
+        assertFalse(new Station("rose", new Coordonnee(1.1, 2.1)).equals(new Station("tulipe")));
+    }
+}
