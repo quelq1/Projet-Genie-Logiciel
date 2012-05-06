983384120c8197bd2470ce4eca2cdc187e8f9437
Loic Cimon <loic.cimon@laposte.net>
2012-05-06 17:50:11 +0200

    Deplacement rechercheItineraire dans une classe a part

--- src/Plan.java
+++ src/Plan.java
@@ -86,122 +86,6 @@ public class Plan {
         return s;
     }
 
-    public Station getStationProche(Coordonnee coord) {
-        Station res = null, tmp;
-        double min, distance;
-
-        Iterator<Station> is = stations.iterator();
-        if (is.hasNext()) {
-            tmp = is.next();
-            res = tmp;
-            min = coord.distance(tmp.getCoord());
-            while (is.hasNext()) {
-                tmp = is.next();
-                distance = coord.distance(tmp.getCoord());
-                if (distance < min) {
-                    res = tmp;
-                    min = distance;
-                }
-            }
-        }
-
-        return res;
-    }
-
-    public ArrayList<Fragment> getDirections(Station s) {
-        ArrayList<Fragment> res = new ArrayList<>();
-        Fragment f;
-
-        //Parcours des lignes
-        Iterator<Ligne> ligne = lignes.iterator();
-        Iterator<Fragment> frag;
-        while (ligne.hasNext()) {
-            //Parcours des fragments de la ligne
-            frag = ligne.next().getListeFragments().iterator();
-            while (frag.hasNext()) {
-                f = frag.next();
-
-                //Si le fragment contient la station et qu'elle n'est pas déjà dans la liste
-                //On ajoute si :
-                // - le fragment contient la station de départ
-                // - le fragment n'est pas déjà contenu
-                // - le fragment n'a pas d'incident
-                // - la station d'arrivée n'a pas d'incident
-
-                if (f.contientStation(s)
-                        && !res.contains(f)
-                        && f.getIncident() == null
-                        && f.getDestination(s).getIncident() == null) {
-                    res.add(f);
-                }
-            }
-        }
-        return res;
-    }
-
-    public void rechercheItineraires(Itineraire itineraire, Station s, Fragment fragPrec, ArrayList<Itineraire> sol) {
-        if (itineraire.getArrivee().equals(s)) {
-            //On fait une copie pour éviter les effets de bords
-            Itineraire tmp = itineraire.clone();
-            //On ne compte pas le temps d'arrêt à la station d'arrivée.
-            tmp.rmDuree(s.getTempsArret());
-            //ajoute l'itinéraire à la liste des solutions
-            sol.add(tmp);
-        } else {
-            //On récupère les directions possibles
-            ArrayList<Fragment> directions = this.getDirections(s);
-            Station dest;
-
-            //On boucle sur les stations possibles
-            for (Fragment fragPossible : directions) {
-                //On récupère la destination du fragment
-                dest = fragPossible.getDestination(s);
-
-                //Vérifie que la station n'a jamais été emprunté
-                if (!itineraire.contains(dest)) {
-                    //enregistrement de la station
-                    itineraire.addStation(dest);
-                    //ajout du temps de trajet
-                    itineraire.addDuree(fragPossible.getTempsDeParcours() + dest.getTempsArret());
-
-                    if (this.aChangement(fragPossible, fragPrec)) {
-                        //incrémente le nombre de changement
-                        itineraire.incrChangement();
-                    }
-                    //appel récursif
-                    this.rechercheItineraires(itineraire, dest, fragPossible, sol);
-
-                    //décrémente le temps de parcours
-                    itineraire.rmDuree(fragPossible.getTempsDeParcours() + dest.getTempsArret());
-                    if (this.aChangement(fragPossible, fragPrec)) {
-                        //décrément le nombre de changement
-                        itineraire.decrChangement();
-                    }
-                    //suppression de la direction et le fragement précédent
-                    itineraire.rmLastStation();
-                }
-            }
-        }
-    }
-
-    public Itineraire getItinerairePlusRapide(Station dep, Station arr) {
-        Itineraire itineraire = new Itineraire(dep, arr);
-        ArrayList<Itineraire> solutions = new ArrayList<>();
-        this.rechercheItineraires(itineraire, dep, null, solutions);
-
-        //On parcours les chemins pour connaître le plus court
-        Itineraire res = null;
-        if (!solutions.isEmpty()) {
-            int min = solutions.get(0).getDuree();
-            for (Itineraire it : solutions) {
-                if (it.getDuree() < min) {
-                    res = it;
-                }
-            }
-        }
-        return res;
-    }
-
     public boolean aChangement(Fragment fragPossible, Fragment prec) {
         if (prec == null || fragPossible == null) {
             return false;
@@ -320,5 +204,27 @@ public class Plan {
                 }
             }
         }
+    }
+    
+    public Station getStationProche(Coordonnee coord) {
+        Station res = null, tmp;
+        double min, distance;
+
+        Iterator<Station> is = stations.iterator();
+        if (is.hasNext()) {
+            tmp = is.next();
+            res = tmp;
+            min = coord.distance(tmp.getCoord());
+            while (is.hasNext()) {
+                tmp = is.next();
+                distance = coord.distance(tmp.getCoord());
+                if (distance < min) {
+                    res = tmp;
+                    min = distance;
+                }
+            }
+        }
+
+        return res;
     }
 }
